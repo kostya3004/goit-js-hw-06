bcf3ef74237c3270d958c839622533fbbf40d567
kostya3004 <110536627+kostya3004@users.noreply.github.com>
2023-01-22 23:16:41 +0100

    task 2 corrected

--- js/task-02.js
+++ js/task-02.js
@@ -6,32 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
-let list = ''
-for (let i = 0; i < ingredients.length; i += 1) {
-  list += `<li class="item">${ingredients[i]}</li>`
-}
-// console.log(list)
-const menu = document.getElementById('ingredients')
-menu.insertAdjacentHTML('afterbegin', list)
-
-console.log(menu)
-
-
-
-// const newElem = document.createElement('li')
-// newElem.textContent = name
-// newElem.classList.add('item')
-
-// ingredients.append(newElem)
-
-// Var 2
-// const newElem = '<li class=item> name </li>'
-// ingredients.insertAdjacentHTML('beforeend', newElem)
-
-
+////// not by task /////
 // let list = ''
-// for (let i=0, i<length, i+=1){
-//   list += '<li class=item> mazafaka ${name}</li>'
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   list += `<li class="item">${ingredients[i]}</li>`
 // }
-// ingredients.insertAdjacentHTML('beforeend', list)
+// // console.log(list)
+// const menu = document.getElementById('ingredients')
+// menu.insertAdjacentHTML('afterbegin', list)
+
+// console.log(menu)
+/////////////////////////////
+
+// By task (need .createElement)
+const list = document.querySelector("#ingredients")
+let listToAppend = [];
+for (let elem of ingredients) {
+  const li = document.createElement("li");
+  li.textContent = elem;
+  li.classList.add("item");
+  listToAppend.push(li)
+}
+list.append(...listToAppend)
